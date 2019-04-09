38487020e2dee3b23c220f012e3878d24697b9f0
Stuart <stuart.lynn@gmail.com>
2019-04-09 06:52:36 -0400

    Adding the schema creation to the populate_data.sh script

--- api/db.js
+++ api/db.js
@@ -32,56 +32,6 @@ export default async () => {
     associate(models);
   });
 
-  //await db.sync({force: true});
-
-  //await models.Project.create({
-    //name: 'New York',
-    //description: 'New Yorks tree census',
-    //img:
-      //'https://www.nationalgeographic.com/content/dam/travel/Guide-Pages/north-america/united-states/newyork/newyork_NationalGeographic_2328428.adapt.1900.1.jpg',
-    //region: {
-      //type: 'Polygon',
-      //coordinates: [
-        //[
-          //[-74.04510498046875, 40.68220016936447],
-          //[-73.88717651367188, 40.68220016936447],
-          //[-73.88717651367188, 40.81380923056958],
-          //[-74.04510498046875, 40.81380923056958],
-          //[-74.04510498046875, 40.68220016936447],
-        //],
-      //],
-      //crs: {type: 'name', properties: {name: 'EPSG:4326'}},
-    //},
-  //});
-
-  //await db.query(
-	//`insert into neighborhoods (name,geom,"createdAt","updatedAt","projectId") ( select  name as name,ST_TRANSFORM(wkb_geometry, 4326) as region, now() as createdAt, now() as updatedAd, 1 as projectId from new_york_neighborhoods);`,
-  //);
-
-  //await db.query(`INSERT INTO 
-	//streets (name,length,osm_id,"isEmpty",state,geom, "createdAt", "updatedAt", "projectId", "neighborhoodId")(
-	  //SELECT nta_name as name, 
-			//ST_LENGTH(wkb_geometry::GEOGRAPHY) as length, 
-			//block_id as osm_id, 
-			//false, 
-			//'TODO', 
-			//wkb_geometry as geom, 
-			//now() as createdAt, 
-			//now() as updatedAt, 
-			//1 as projectId, 
-			//(
-				//SELECT neighborhoods.id  as id
-				//FROM neighborhoods,streets
-				//WHERE ST_Contains(neighborhoods.geom, ST_CENTROID(streets.geom))
-				//limit 1 
-				//) as neighborhoodId 
-	  //FROM new_york_blocks 
-	  //WHERE ST_GeometryType(wkb_geometry) = 'ST_LineString');`);
-	  
-  //await db.query(`
-	//UPDATE streets SET  "neighborhoodId"  = neighborhoods.id  
-	//FROM neighborhoods
-	//WHERE ST_Contains(neighborhoods.geom, ST_CENTROID(streets.geom))
-  //`)
+  
   return [models, db];
 };
--- populate_data.sh
+++ populate_data.sh
@@ -6,6 +6,6 @@ docker run -it --rm -v $(pwd)/data:/data --network treekitnewprototype_treekit d
 echo "importing NY Neighborhoods"
 docker run -it --rm -v $(pwd)/data:/data --network treekitnewprototype_treekit data_import -f "PostgreSQL" PG:"dbname=docker user=docker host=database password=docker" -lco OVERWRITE=yes -nlt PROMOTE_TO_MULTI -nln new_york_neighborhoods /data/zillow/ZillowNeighborhoods-NY.shp
 
-echo "Making derived tables"
 
-PGPASSWORD=docker psql -h database -U docker -d docker -c "insert into neighborhoods (id,name,region,\"createdAt\",\"updatedAt\",\"projectId\") ( select  regionid::INTEGER as id, name as name,ST_TRANSFORM(wkb_geometry, 4326) as region, now() as createdAt, now() as updatedAd, 1 as projectId from new_york_neighborhoods);"
+echo "Setting up db tables"
+docker-compose run api babel-node initalizeDB.js
