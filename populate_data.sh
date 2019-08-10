b0dc2225881a3f4336d11276d17ba64ca13080cb
Stuart Lynn <stuart.lynn@gmail.com>
2019-08-10 13:54:54 -0400

    New docker image for loading data

--- populate_data.sh
+++ populate_data.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "importing NY blocks"
-docker run -it --rm -v $(pwd)/data:/data --network treekitnewprototype_treekit data_import -f "PostgreSQL" PG:"dbname=docker user=docker host=database password=docker" -lco OVERWRITE=yes -nln new_york_blocks  /data/2015\ Street\ Tree\ Census\ -\ Blockface\ Data.geojson
+docker run -it --rm -v $(pwd)/data:/data --network treekit_treekit --entrypoint=ogr2ogr stuartlynn/ogr2ogr -f "PostgreSQL" PG:"dbname=docker user=docker host=database password=docker" -lco OVERWRITE=yes -nln new_york_blocks  /data/2015\ Street\ Tree\ Census\ -\ Blockface\ Data.geojson
 
 echo "importing NY Neighborhoods"
-docker run -it --rm -v $(pwd)/data:/data --network treekitnewprototype_treekit data_import -f "PostgreSQL" PG:"dbname=docker user=docker host=database password=docker" -lco OVERWRITE=yes -nlt PROMOTE_TO_MULTI -nln new_york_neighborhoods /data/zillow/ZillowNeighborhoods-NY.shp
+docker run -it --rm -v $(pwd)/data:/data --network treekit_treekit --entrypoint=ogr2ogr stuartlynn/ogr2ogr -f "PostgreSQL" PG:"dbname=docker user=docker host=database password=docker" -lco OVERWRITE=yes -nlt PROMOTE_TO_MULTI -nln new_york_neighborhoods /data/zillow/ZillowNeighborhoods-NY.shp
 
 
 echo "Setting up db tables"
